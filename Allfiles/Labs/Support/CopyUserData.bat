9d557d8983a46f6a6eed6588fefde2e07e8ce3fc
Brian Hinkelman <brhinkel@microsoft.com>
2019-09-26 15:06:22 -0700

    update

---
+++ Allfiles/Labs/Support/CopyUserData.bat
@@ -0,0 +1,6 @@
+md c:\Data
+copy \\lon-dc1\e$\labfiles\Support\Data c:\Data
+md c:\Misc
+copy \\lon-dc1\e$\labfiles\Support\Misc c:\Misc
+md c:\Reports
+copy \\lon-dc1\e$\labfiles\Support\Reports c:\Reports
